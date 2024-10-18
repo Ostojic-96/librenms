0627666ac4de34fcfdd3d2cdd50aa5eb628029b7
Ivan Ostojic <ivan.ostojic@sum.ba>
2024-10-18 12:42:13 +0200

    remove maintance column

--- app/Http/Controllers/Table/DeviceController.php
+++ app/Http/Controllers/Table/DeviceController.php
@@ -86,7 +86,7 @@ class DeviceController extends TableController
             'status' => 'status',
             'icon' => 'icon',
             'hostname' => 'hostname',
-            // 18.10.24 - M. Coric - isključujem 'display' => 'display', // 17.10.2024 - Osta dodao da radi sort i po ovoj koloni iz tabele devices
+	    // 18.10.24 - M. Coric - isključujem 'display' => 'display', // 17.10.2024 - Osta dodao da radi sort i po ovoj koloni iz tabele devices
             'hardware' => 'hardware',
             'os' => 'os',
             'uptime' => \DB::raw('IF(`status` = 1, `uptime`, `last_polled` - NOW())'),
